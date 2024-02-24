95af3eda9e1d28d34a29afcbc37794a1d28ff8f5
Abhinandan <jhaabhinandan55@gmail.com>
2024-02-24 14:21:18 +0530

    Side Bar under progress

---
+++ src/components/ConversationsItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ConversationsItem() {
+  return (
+    <div>
+      Conversations
+    </div>
+  )
+}
+
+export default ConversationsItem
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,48 @@
 import React from 'react';
 import "./myStyles.css";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
+import GroupAddIcon from '@mui/icons-material/GroupAdd';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import SearchIcon from '@mui/icons-material/Search';
+import { IconButton } from '@mui/material';
+import ConversationsItem from './ConversationsItem';
 
 function Sidebar() {
   return (
     <div className='sidebar-container'>
-      
+      <div className='sb-header'>
+        <div>
+          <IconButton>
+            <AccountCircleIcon/>
+          </IconButton>
+        </div>
+
+        <div>
+          <IconButton>
+            <PersonAddAltIcon/>
+          </IconButton>
+          <IconButton>
+            <GroupAddIcon/>
+          </IconButton>
+          <IconButton>
+            <AddCircleOutlineIcon/>
+          </IconButton>
+          <IconButton>
+            <DarkModeIcon/>
+          </IconButton>
+        </div>
+      </div>
+      <div className='sb-search'>
+        <IconButton>
+          <SearchIcon/>
+        </IconButton>
+        <input placeholder='Search' className='search-box'/>
+      </div>
+      <div className='sb-conversations'>
+        <ConversationsItem/>
+      </div>
     </div>
   )
 }
